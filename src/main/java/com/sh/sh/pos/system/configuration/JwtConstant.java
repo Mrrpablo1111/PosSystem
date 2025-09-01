e176a45ea98c7ef482fb853f997bbc9ea4e893b8
Seng Hong <hongseng586@gmail.com>
2025-09-01 22:28:15 +0700

    project-init

---
+++ src/main/java/com/sh/sh/pos/system/configuration/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.sh.sh.pos.system.configuration;
+
+public class JwtConstant {
+    public static String JWT_SECRET = "dfalkdfjalsdfjsaf,dfsadfaksdfasf, dsfadfafasf,";
+    public static String JWT_HEADER = "Authorization";
+}
