f6021f4473923dba4356d66bccb2c74ef543d2d8
Seng Hong <hongseng586@gmail.com>
2025-12-18 15:52:30 +0700

    project-init

--- src/main/java/com/sh/sh/pos/system/service/BranchService.java
+++ src/main/java/com/sh/sh/pos/system/service/BranchService.java
@@ -3,13 +3,12 @@ package com.sh.sh.pos.system.service;
 import java.util.List;
 
 import com.sh.sh.pos.system.exceptions.UserException;
-import com.sh.sh.pos.system.model.User;
 import com.sh.sh.pos.system.payload.dto.BranchDTO;
 
 public interface BranchService {
 	
 	BranchDTO createBranch(BranchDTO branchDTO) throws UserException;
-	BranchDTO updateBranch(Long id, BranchDTO branchDTO, User user) throws Exception;
+	BranchDTO updateBranch(Long id, BranchDTO branchDTO) throws Exception;
 	void deleteBranch(Long id) throws Exception;
 	
 	List<BranchDTO> getAllBranchesByStoreId(Long storeId);
