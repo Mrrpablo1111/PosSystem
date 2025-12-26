594501eae0403ccdec6c150f00a7ba4e6f75140a
Seng Hong <hongseng586@gmail.com>
2025-12-26 15:47:55 +0700

    project-init

--- src/main/java/com/sh/sh/pos/system/service/RefundService.java
+++ src/main/java/com/sh/sh/pos/system/service/RefundService.java
@@ -3,17 +3,16 @@ package com.sh.sh.pos.system.service;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import com.sh.sh.pos.system.model.Refund;
 import com.sh.sh.pos.system.payload.dto.RefundDTO;
 
 public interface RefundService {
 	
-	RefundDTO createRefund(Refund refund) throws Exception;
+	RefundDTO createRefund(RefundDTO refund) throws Exception;
 	
 	List<RefundDTO> getAllRefunds() throws Exception;
 	
-	RefundDTO getRefundByCashier(Long cashierId) throws Exception;
-	RefundDTO getRefundByShiftReport(Long shiftReportId) throws Exception;
+	List<RefundDTO> getRefundByCashier(Long cashierId) throws Exception;
+    List<RefundDTO> getRefundByShiftReport(Long shiftReportId) throws Exception;
 	
 	List<RefundDTO> getRefundByCashierAndDateRang(Long cashierId, LocalDateTime startDate, LocalDateTime endDate) throws Exception;
 	
--- src/main/java/com/sh/sh/pos/system/service/serviceImpl/RefundServiceImpl.java
+++ src/main/java/com/sh/sh/pos/system/service/serviceImpl/RefundServiceImpl.java
@@ -2,66 +2,98 @@ package com.sh.sh.pos.system.service.serviceImpl;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.sh.sh.pos.system.mapper.RefundMapper;
+import com.sh.sh.pos.system.model.Branch;
+import com.sh.sh.pos.system.model.Order;
 import com.sh.sh.pos.system.model.Refund;
+import com.sh.sh.pos.system.model.User;
 import com.sh.sh.pos.system.payload.dto.RefundDTO;
+import com.sh.sh.pos.system.repository.OrderRepository;
+import com.sh.sh.pos.system.repository.RefundRepository;
 import com.sh.sh.pos.system.service.RefundService;
+import com.sh.sh.pos.system.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class RefundServiceImpl implements RefundService{
-
+	
+	private final UserService userService;
+	private final OrderRepository orderRepository;
+	private final RefundRepository refundRepository;
+	
 	@Override
-	public RefundDTO createRefund(Refund refund) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public RefundDTO createRefund(RefundDTO refund) throws Exception {
+		User cashier = userService.getCurrentUser();
+		
+		Order order = orderRepository.findById(refund.getOrderId()).orElseThrow(() -> new Exception("order not found"));
+		
+		Branch branch = order.getBranch();
+		
+		Refund createRefund = Refund.builder()
+				.order(order)
+				.cashier(cashier)
+				.branch(branch)
+				.reason(refund.getReason())
+				.amount(refund.getAmount())
+				.createdAt(refund.getCreatedAt())
+				.build();
+		
+		Refund savedRefund = refundRepository.save(createRefund);
+ 				 
+		return RefundMapper.toDTO(savedRefund);
 	}
 
 	@Override
 	public List<RefundDTO> getAllRefunds() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return refundRepository.findAll().stream().map(RefundMapper::toDTO).collect(Collectors.toList());
+		
 	}
 
 	@Override
-	public RefundDTO getRefundByCashier(Long cashierId) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public List<RefundDTO>  getRefundByCashier(Long cashierId) throws Exception {
+		
+		return refundRepository.findByCashierId(cashierId).stream().map(RefundMapper::toDTO).collect(Collectors.toList());
 	}
 
 	@Override
-	public RefundDTO getRefundByShiftReport(Long shiftReportId) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public List<RefundDTO> getRefundByShiftReport(Long shiftReportId) throws Exception {
+		return refundRepository.findByShiftReportId(shiftReportId).stream().map(RefundMapper::toDTO).collect(Collectors.toList());
 	}
 
 	@Override
 	public List<RefundDTO> getRefundByCashierAndDateRang(Long cashierId, LocalDateTime startDate, LocalDateTime endDate)
 			throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	
+		return refundRepository.findByCashierIdAndCreatedAtBetween(cashierId, startDate, endDate).stream().map(RefundMapper::toDTO).collect(Collectors.toList()) ;
 	}
 
 	@Override
 	public List<RefundDTO> getRefundByBranch(Long branchId) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return refundRepository.findByBranchId(branchId).stream().map(RefundMapper::toDTO).collect(Collectors.toList());
 	}
 
 	@Override
 	public RefundDTO getRefundById(Long refundId) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		
+		
+		return refundRepository.findById(refundId).map(RefundMapper::toDTO).orElseThrow(()-> new Exception("Refund not found"));
+		
 	}
 
 	@Override
 	public void deleteRefund(Long refundId) throws Exception {
-		// TODO Auto-generated method stub
+		this.getRefundById(refundId);
+		refundRepository.deleteById(refundId);
+		
 		
 	}
+
 
 }
